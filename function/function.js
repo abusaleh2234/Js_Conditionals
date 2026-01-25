447b8754f0491858906ec217c6ce125cba36ac0e
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-01-25 21:32:15 +0600

    task 3 commit

--- function/function.js
+++ function/function.js
@@ -1,4 +1,4 @@
-
+// ///////////////Task 1
 function multiply(num1, num2, num3, num4){
     const multi = num1* num2* num3* num4
     return multi
@@ -9,7 +9,7 @@ const z = 20;
 const n = 30;
 const multipl = multiply(x, y, z,n,)
 // console.log(multipl);
-
+// ///////////////  task 2
 function oddOrEven(number){
     if (number % 2 !== 0) {
         return number * 2
@@ -22,3 +22,23 @@ function oddOrEven(number){
 const numnum = 6;
 const result = oddOrEven(numnum) 
 // console.log(result);
+
+
+// ///////////// Task 3
+
+function average(numbers){
+    // console.log(numbers);
+    let sum = 0
+    for (const number of numbers) {
+        // console.log(number);
+        sum = sum + number
+    }
+    console.log(sum ,numbers.length);
+    return sum / numbers.length
+    
+}
+
+const ages = [12,23,16,43,50,26]
+const avg = average(ages)
+console.log(avg);
+
