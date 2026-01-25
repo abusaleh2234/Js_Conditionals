3f743838dee06b19f1e004d5c84fd4651e2d8d74
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-01-25 21:14:41 +0600

    task one

---
+++ function/function.js
@@ -0,0 +1,11 @@
+
+function multiply(num1, num2, num3, num4){
+    const multi = num1* num2* num3* num4
+    return multi
+}
+const x = 10;
+const y = 12;
+const z = 20;
+const n = 30;
+const multipl = multiply(x, y, z,n,)
+// console.log(multipl);
